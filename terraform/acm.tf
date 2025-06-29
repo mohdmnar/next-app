b8cd2ae946085d6bf36b836ccc7766d02d0a582d
Aaron Taylor <aaron.taylor@yoono.io>
2025-06-29 15:42:59 +0100

    update validation record

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -15,14 +15,19 @@ resource "aws_route53_record" "cert_validation" {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
+      zone   = replace(dvo.domain_name, "/^www\\./", "")  # Extract base domain
     }
   }
 
-  zone_id         = contains(keys(each.value), "zengech.com") ? aws_route53_zone.primary_com.zone_id : aws_route53_zone.primary_uk.zone_id
+  zone_id = (
+    endswith(each.value.zone, "zengech.com") 
+    ? aws_route53_zone.primary_com.zone_id 
+    : aws_route53_zone.primary_uk.zone_id
+  )
   name            = each.value.name
   type            = each.value.type
   records         = [each.value.record]
-  ttl             = 300
+  ttl             = 60
   allow_overwrite = true
 }
 
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -55,25 +55,3 @@ resource "aws_route53_record" "www_com" {
     evaluate_target_health = false
   }
 }
-
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.zengech.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-      zone   = replace(dvo.domain_name, "/^www\\./", "")  # Extract base domain
-    }
-  }
-
-  zone_id = (
-    endswith(each.value.zone, "zengech.com") 
-    ? aws_route53_zone.primary_com.zone_id 
-    : aws_route53_zone.primary_uk.zone_id
-  )
-  name            = each.value.name
-  type            = each.value.type
-  records         = [each.value.record]
-  ttl             = 60
-  allow_overwrite = true
-}
