cdb06581aa0fbeb31c8aae644adcfaf51940a3a2
mohdmnar <mohdmnar@amazon.com>
2025-08-28 02:15:26 +0530

    changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 // pages/index.jsx
-import App from '../src/app'  // Import from src directory
+import App from '/src/app'  // Import from src directory
 
 export default function Home() {
   return <App />
